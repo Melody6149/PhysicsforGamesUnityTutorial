59bcf8d44923ed6677683e0a651b217afc794421
Melody <reid.eastin@gmail.com>
2020-09-10 15:11:30 -0500

    I added some things while following the tutorial

--- Assets/Scripts/PhysicsObjectBehavior.cs
+++ Assets/Scripts/PhysicsObjectBehavior.cs
@@ -9,16 +9,33 @@ public class PhysicsObjectBehavior : MonoBehaviour
     public Material awakeMaterial = null;
     public Material asleepMaterial = null;
 
-    private Rigidbody rigidbody = null;
+    private Rigidbody _rigidbody = null;
+
+    bool wasSleeping = false;
     // Start is called before the first frame update
     void Start()
     {
-     
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void FixedUpdate()
+    {
+        if(_rigidbody.IsSleeping() && !wasSleeping && asleepMaterial != null)
+        {
+            wasSleeping = true;
+            GetComponent<MeshRenderer>().material = asleepMaterial;
+        }
+
+        if(!_rigidbody.IsSleeping() && wasSleeping && awakeMaterial != null)
+        {
+            wasSleeping = false;
+            GetComponent<MeshRenderer>().material = awakeMaterial;
+        }
     }
 }
