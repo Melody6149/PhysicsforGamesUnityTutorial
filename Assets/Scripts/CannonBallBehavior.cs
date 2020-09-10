934954cdf2a5eefc18a4187a3a7cd125528d3e1a
Melody <reid.eastin@gmail.com>
2020-09-10 14:17:24 -0500

    Initial commit

---
+++ Assets/Scripts/CannonBallBehavior.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class CannonBallBehavior : MonoBehaviour
+{
+    public float forceOnFire = 300;
+
+    bool fire = false;
+    bool canFire = true;
+
+    Rigidbody rigidbody = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown && canFire)
+        {
+            rigidbody.isKinematic = false;
+            rigidbody.AddForce(transform.forward * forceOnFire);
+            canFire = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PhysicsObjectBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PhysicsObjectBehavior : MonoBehaviour
+{
+    public Material awakeMaterial = null;
+    public Material asleepMaterial = null;
+
+    private Rigidbody rigidbody = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
