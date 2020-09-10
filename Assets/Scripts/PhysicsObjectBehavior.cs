981204711b0844cdb08bc4a62f2da4a470c4c0a3
Melody <reid.eastin@gmail.com>
2020-09-10 15:13:46 -0500

    Revert "I added some things while following the tutorial"
    This reverts commit 59bcf8d44923ed6677683e0a651b217afc794421.

--- Assets/Scripts/PhysicsObjectBehavior.cs
+++ Assets/Scripts/PhysicsObjectBehavior.cs
@@ -9,33 +9,16 @@ public class PhysicsObjectBehavior : MonoBehaviour
     public Material awakeMaterial = null;
     public Material asleepMaterial = null;
 
-    private Rigidbody _rigidbody = null;
-
-    bool wasSleeping = false;
+    private Rigidbody rigidbody = null;
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    private void FixedUpdate()
-    {
-        if(_rigidbody.IsSleeping() && !wasSleeping && asleepMaterial != null)
-        {
-            wasSleeping = true;
-            GetComponent<MeshRenderer>().material = asleepMaterial;
-        }
-
-        if(!_rigidbody.IsSleeping() && wasSleeping && awakeMaterial != null)
-        {
-            wasSleeping = false;
-            GetComponent<MeshRenderer>().material = awakeMaterial;
-        }
+        
     }
 }
